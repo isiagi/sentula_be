7e93a14e076caa20da932ce423d9664fca2194c1
isiagi <isiagigeofrey0@gmail.com>
2024-06-05 14:03:46 +0300

    chore: Change model fields to decimal

--- server/loan/models.py
+++ server/loan/models.py
@@ -7,9 +7,9 @@ class Loan(models.Model):
     member_id = models.ForeignKey(User, on_delete=models.CASCADE,to_field="username", blank=True, null=True, related_name="member") # membership_id of user model
     type = models.CharField(max_length=100)
     plan = models.CharField(max_length=100)
-    amount = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
     granteers = models.CharField(max_length=100)
-    remaining_amount = models.CharField(max_length=100, blank=True)
+    remaining_amount = models.DecimalField(max_digits=10, decimal_places=2)
     reference_no = models.CharField(max_length=100, blank=True, unique=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
--- server/saving/models.py
+++ server/saving/models.py
@@ -7,7 +7,7 @@ class Saving(models.Model):
     member_id = models.ForeignKey(User, on_delete=models.CASCADE, to_field="username", related_name="+",null=True, blank=True)  # User model field {foreign key}
     member_name = models.CharField(null=True, max_length=100)
     account_number = models.CharField(null=True, max_length=100)
-    amount = models.CharField(null=True, max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2,null=True, blank=True)
     date_of_payment = models.DateTimeField(null=True, blank=True)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
