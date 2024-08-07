f5e63876243308aa7ff70a1164f385b0a1ddbdf9
isiagi <isiagigeofrey0@gmail.com>
2024-08-07 12:33:33 +0300

    chore: set back debug false

--- server/server/settings.py
+++ server/server/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7279xm&pj_!(x)@e079-^nh*=)mxx3#a!66$_d_y$k5k_@60f='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['10.0.2.2', '127.0.0.1', 'agalyawamm-backend.onrender.com', 'ada-backend-6cir.onrender.com']
 
