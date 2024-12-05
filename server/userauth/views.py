800455b278ece0838f66a04a1efd0865d6b0cfe8
isiagi <isiagigeofrey0@gmail.com>
2024-12-05 15:01:42 +0300

    chore: Fix reset password

--- server/userauth/views.py
+++ server/userauth/views.py
@@ -301,7 +301,7 @@ def createpassword(request):
         # Save user
         user.save()
 
-        token = Token.objects.get_or_create(user=user)
+        token, created = Token.objects.get_or_create(user=user)
 
         nv = UserSerializer(user)
 
